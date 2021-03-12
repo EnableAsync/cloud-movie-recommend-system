5a6ed77678bcf4cc867d6aa9e6892a5df0c32f1d
klaus125 <klaus125@qq.com>
2021-03-12 22:52:57 +0800

    feat: add openfeign func

--- consumer-recommender80/src/main/java/com/klaus/consumer/Recommender80.java
+++ consumer-recommender80/src/main/java/com/klaus/consumer/Recommender80.java
@@ -2,8 +2,10 @@ package com.klaus.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Recommender80 {
     public static void main(String[]args){
         SpringApplication.run(Recommender80.class,args);
