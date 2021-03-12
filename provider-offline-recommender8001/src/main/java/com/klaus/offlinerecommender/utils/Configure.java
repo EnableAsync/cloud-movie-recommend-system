9a27d0940f8fae11ce49d77afc0b589468ad0142
klaus125 <klaus125@qq.com>
2021-03-12 22:53:36 +0800

    fix: changed configuration

--- provider-offline-recommender8001/src/main/java/com/klaus/offlinerecommender/utils/Configure.java
+++ provider-offline-recommender8001/src/main/java/com/klaus/offlinerecommender/utils/Configure.java
@@ -1,14 +1,17 @@
 package com.klaus.offlinerecommender.utils;
 
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configure {
 
-    private String mongoHost = "192.168.40.137";
-    private int mongoPort = 27017;
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoHost;
+    @Value("${spring.data.mongodb.port}")
+    private int mongoPort;
 
 
     @Bean(name = "mongoClient")
