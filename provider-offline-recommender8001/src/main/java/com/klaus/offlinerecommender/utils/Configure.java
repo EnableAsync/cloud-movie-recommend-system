da0cb99a974fada4345031bd7da781c599f004aa
rai <rai>
2021-03-12 22:25:36 +0800

    update: Constant.java and mongodb url and port

--- provider-offline-recommender8001/src/main/java/com/klaus/offlinerecommender/utils/Configure.java
+++ provider-offline-recommender8001/src/main/java/com/klaus/offlinerecommender/utils/Configure.java
@@ -1,19 +1,24 @@
 package com.klaus.offlinerecommender.utils;
 
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configure {
 
-    private String mongoHost = "192.168.40.137";
-    private int mongoPort = 27017;
+    @Value("${spring.data.mongodb.uri}")
+
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private int mongoPort;
 
 
     @Bean(name = "mongoClient")
-    public MongoClient getMongoClient(){
-        return new MongoClient( mongoHost , mongoPort );
+    public MongoClient getMongoClient() {
+        return new MongoClient(mongoHost, mongoPort);
     }
 
 
